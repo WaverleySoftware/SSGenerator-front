763d329efe786ff36a9219b1a460b6e7e0dc72ff
Artem Oktysiuk <aoktysiuk@waverleysoftware.com>
2021-10-21 14:10:53 +0300

    remove unused component

--- projects/steam-generation-assessment-lib/src/app/app.module.ts
+++ projects/steam-generation-assessment-lib/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from "@angular/common";
 import { BlockUIModule } from "ng-block-ui";
 import { ProjectsJobsModule, SizingSharedLibModule } from "sizing-shared-lib";
 import { SteamGenerationAssessmentComponent } from './steam-generation-assessment/steam-generation-assessment.component';
-import { SgaStepsComponent } from './sga-steps/sga-steps.component';
 import { SharedModule } from "./modules/shared/shared.module";
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { SgaInputParametersComponent } from './sga-input-parameters/sga-input-parameters.component';
@@ -12,7 +11,6 @@ import { SgaInputParametersComponent } from './sga-input-parameters/sga-input-pa
 @NgModule({
   declarations: [
     SteamGenerationAssessmentComponent,
-    SgaStepsComponent,
     SgaInputParametersComponent
   ],
 	imports: [
