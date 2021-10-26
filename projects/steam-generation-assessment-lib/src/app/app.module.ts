5168ae200eccb8a0735ab5e717f3f13b463e10f6
Artem Oktysiuk <aoktysiuk@waverleysoftware.com>
2021-10-26 17:24:21 +0300

    [Final Proposal] start page creation

--- projects/steam-generation-assessment-lib/src/app/app.module.ts
+++ projects/steam-generation-assessment-lib/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { SgaInputParametersComponent } from './sga-input-parameters/sga-input-parameters.component';
 import { SgaBenchmarkComponent } from './sga-benchmark/sga-benchmark.component';
 import { SgaProposedSetupComponent } from './sga-proposed-setup/sga-proposed-setup.component';
+import { SgaFinalProposalComponent } from './sga-final-proposal/sga-final-proposal.component';
 
 @NgModule({
   declarations: [
     SteamGenerationAssessmentComponent,
     SgaInputParametersComponent,
     SgaBenchmarkComponent,
-    SgaProposedSetupComponent
+    SgaProposedSetupComponent,
+    SgaFinalProposalComponent
   ],
 	imports: [
 		CommonModule,
