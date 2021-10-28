794525773b4bbd5f67d011e49b624f6eb148f5d0
Artem Oktysiuk <aoktysiuk@waverleysoftware.com>
2021-10-28 16:57:05 +0300

    [Boiler Scheme] add base boiler image

--- projects/steam-generation-assessment-lib/src/app/sga-input-parameters/sga-input-parameters.component.ts
+++ projects/steam-generation-assessment-lib/src/app/sga-input-parameters/sga-input-parameters.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators
+} from "@angular/forms";
 
 @Component({
   selector: 'app-sga-input-parameters',
@@ -7,28 +13,32 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
   styleUrls: ['./sga-input-parameters.component.scss']
 })
 export class SgaInputParametersComponent implements OnInit {
-
   utilityParamsForm: FormGroup;
   boilerParamsForm: FormGroup;
 
   public deaeratorType: string = 'autmosphericDeaerator';
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {
+    this.initForm();
+  }
 
   ngOnInit() {
-    this.initForm();
+  }
+
+  submitForm(): void {
+    console.log(this.utilityParamsForm.getRawValue());
   }
 
   private initForm():void {
     this.utilityParamsForm = this.fb.group({
       HOURS_OF_OPERATION: ["", Validators.required],
       FUEL_TYPE: ["Natural Gas", Validators.required],
-      FUEL_CALORIFIC_VALUE: ["", Validators.required],
+      FUEL_CALORIFIC_VALUE: ["", [Validators.required, Validators.min(1), Validators.max(100)]],
       cO2EmissionsUnitFuel: ["0.1850", Validators.required],
       costOfFuelUnit: ["0.025"],
-      isFuelComsumptionMeasured: [false],
-      costOfFuelYear: [""],
-      fuelConsumptionYear: [""],
+      IS_FUEL_COMSUMPTION_MEASURED: [false],
+      COST_OF_FUEL_YEAR: [""],
+      FUEL_CONSUMPTION_YEAR: [""],
       areCO2OrCarbonEmissionsTaxed: [false],
       carbonLeviTaxUnit: [""],
       costOfCo2UnitMax: [""],
@@ -44,4 +54,35 @@ export class SgaInputParametersComponent implements OnInit {
     })
   }
 
+  public fieldValidation(fieldName: string): string {
+    let errors: string;
+    const control: AbstractControl = this.utilityParamsForm.get(fieldName);
+    const controlErrors: ValidationErrors = control.errors;
+
+    if (control.invalid && control.touched && controlErrors !== null) {
+      Object.keys(controlErrors).forEach(keyError => {
+        switch (keyError) {
+          case 'required': {
+            errors = keyError;
+            break;
+          }
+          case 'min': {
+            errors = `Min: ${controlErrors[keyError].min}`;
+            break;
+          }
+          case 'max': {
+            errors = `Max: ${controlErrors[keyError].max}`;
+            break;
+          }
+        }
+      });
+    }
+
+    return errors;
+  }
+
+  public isRequired(fieldName: string): boolean {
+    return this.utilityParamsForm.get(fieldName).errors
+      && this.utilityParamsForm.get(fieldName).errors.required;
+  }
 }
