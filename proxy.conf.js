c6854cf80ad82e93a7aeab2d0a80105a0f9f8b80
Artem Oktysiuk <aoktysiuk@waverleysoftware.com>
2022-01-17 16:39:13 +0200

    [FIXES] try to fix qa(4)

--- proxy.conf.js
+++ proxy.conf.js
@@ -2,6 +2,7 @@ const PROXY_CONFIG = [
   {
     context: ['/Api', '/api'],
     target: 'http://94.130.206.72:3080',
+    // target: 'http://94.130.206.72:2080',
     secure: false,
     logLevel: "debug",
     changeOrigin: true
