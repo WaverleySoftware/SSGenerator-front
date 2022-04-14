ea691c19d7e88a5406a721b3f73f79b291d5fc79
Artem Oktysiuk <aoktysiuk@waverleysoftware.com>
2022-04-14 12:19:37 +0300

    final proposal

--- projects/steam-generation-assessment-lib/src/app/tabs/sga-final-proposal/sga-final-proposal.component.ts
+++ projects/steam-generation-assessment-lib/src/app/tabs/sga-final-proposal/sga-final-proposal.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 import { ChartBarDataInterface } from '../../interfaces/chart-bar.interface';
 import { horizontalChart, verticalChart, verticalChartLabels } from '../../utils/proposed-setup-def-data';
 import { BenchmarkResBenchmarkInterface } from "../../interfaces/calc-benchmark-res.interface";
+import { TranslationService, Enumeration } from "sizing-shared-lib";
 
 @Component({
   selector: 'app-sga-final-proposal',
   templateUrl: './sga-final-proposal.component.html',
   styleUrls: ['./sga-final-proposal.component.scss']
 })
-export class SgaFinalProposalComponent {
+export class SgaFinalProposalComponent implements OnInit {
   @Input() current: Partial<BenchmarkResBenchmarkInterface>; // Benchmark
   @Input() potential: Partial<BenchmarkResBenchmarkInterface>; // Overall...
   @Input() currency: string;
@@ -16,9 +17,19 @@ export class SgaFinalProposalComponent {
   @Input() horizontalChart: ChartBarDataInterface[] = horizontalChart;
   @Input() units: { [key: number]: string };
   chartLabels: string[] = verticalChartLabels;
+  get fuelTypeList() {
+    if (this.translationService.displayGroup && this.translationService.displayGroup.enumerations) {
+      const enumeration: Enumeration = this.translationService.displayGroup.enumerations
+        .find(({enumerationName}) => enumerationName === "FuelTypeList_BoilerHouseInput");
+      return enumeration && enumeration.enumerationDefinitions &&
+        enumeration.enumerationDefinitions.reduce((acc, {masterTextKey, id}) => ({...acc, [id]: masterTextKey}), {});
+    }
+    return null;
+  }
 
-  constructor() {}
+  constructor(protected translationService: TranslationService) {}
 
+  ngOnInit() {}
 
   savingPercent(current: number, potential: number): number {
     if (!current || !potential) {
