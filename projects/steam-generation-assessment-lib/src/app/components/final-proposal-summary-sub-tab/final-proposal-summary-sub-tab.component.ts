9851e3301d02ff6ee3f79364c27d34ab5cf61a42
Artem Oktysiuk <aoktysiuk@waverleysoftware.com>
2022-05-26 12:39:22 +0300

    SG-213

--- projects/steam-generation-assessment-lib/src/app/components/final-proposal-summary-sub-tab/final-proposal-summary-sub-tab.component.ts
+++ projects/steam-generation-assessment-lib/src/app/components/final-proposal-summary-sub-tab/final-proposal-summary-sub-tab.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from "@angular/core";
 import { BenchmarkResBenchmarkInterface } from "../../interfaces/calc-benchmark-res.interface";
+import { SelectedUnitsInterface } from "../../interfaces/selectedUnits.interface";
 
 @Component({
   selector: 'app-final-proposal-summary-sub-tab',
@@ -9,14 +10,34 @@ import { BenchmarkResBenchmarkInterface } from "../../interfaces/calc-benchmark-
 })
 export class FinalProposalSummarySubTabComponent implements OnInit {
   @Input() current: BenchmarkResBenchmarkInterface;
+  @Input() originalCurrent: BenchmarkResBenchmarkInterface;
   @Input() potential: BenchmarkResBenchmarkInterface;
+  @Input() originalPotential: BenchmarkResBenchmarkInterface;
   @Input() units: { [key: number]: string };
   @Input() currency: string;
   @Input() fuelTypeList: { [key: string]: string };
+  @Input() selectedUnits: SelectedUnitsInterface;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+	get massFlowUnit(): string {
+		if (
+			!this.units ||
+			!this.selectedUnits ||
+			!this.selectedUnits.massFlowUnitSelected ||
+			!this.units[this.selectedUnits.massFlowUnitSelected]
+		) return '-';
+
+		return this.units[this.selectedUnits.massFlowUnitSelected].replace('/yr', '')
+	}
+
+	get fuelEnergyInGJ(): {current: number; potential: number } {
+		return {
+			current: this.originalCurrent && this.originalCurrent.qfuel * 0.0036 || 0,
+			potential: this.originalPotential && this.originalPotential.qfuel * 0.0036 || 0
+		}
+	}
 }
